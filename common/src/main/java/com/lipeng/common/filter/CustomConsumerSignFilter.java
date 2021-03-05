2f59a2a71600ecfdcaa187c4dbd9531003338f11
lipeng <lp960419@163.com>
2021-03-05 11:27:37 +0800

    commit

---
+++ common/src/main/java/com/lipeng/common/filter/CustomConsumerSignFilter.java
@@ -0,0 +1,31 @@
+package com.lipeng.common.filter;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.*;
+import com.lipeng.common.utils.twitter.SnowflakeIdUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.DigestUtils;
+
+/**
+ * @Author: lipeng
+ * @Date: 2021/03/05 10:26
+ */
+@Slf4j
+@Activate(group = Constants.CONSUMER, order = -10000)
+public class CustomConsumerSignFilter implements Filter {
+
+    public static final String SIGN = "sign";
+    public static final String TRACEID = "traceId";
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        String sign = DigestUtils.md5DigestAsHex(invoker.getUrl().getServiceKey().getBytes());
+        String traceId = SnowflakeIdUtils.nextId();
+        RpcContext.getContext().setAttachment(SIGN, sign);
+        RpcContext.getContext().setAttachment(TRACEID, traceId);
+        log.info("DUBBO消费端参数透传:[traceId:{},sign:{}]", traceId, sign);
+        return invoker.invoke(invocation);
+    }
+
+}
--- common/src/main/java/com/lipeng/common/filter/CustomProviderLogFilter.java
+++ common/src/main/java/com/lipeng/common/filter/CustomProviderLogFilter.java
@@ -5,6 +5,7 @@ import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.DigestUtils;
 
 /**
  * @Author: lipeng
@@ -12,14 +13,19 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Activate(group = Constants.PROVIDER, order = 2)
 @Slf4j
-public class CustomTraceFilter implements Filter {
+public class CustomProviderLogFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        String name = invoker.getInterface().getName();
+        String traceId = RpcContext.getContext().getAttachments().get(CustomConsumerSignFilter.TRACEID);
+        String sign = RpcContext.getContext().getAttachments().get(CustomConsumerSignFilter.SIGN);
+        String signNew = DigestUtils.md5DigestAsHex(invoker.getUrl().getServiceKey().getBytes());
+        String serviceKey = invoker.getUrl().getServiceKey();
         Object[] args = invocation.getArguments();
-        String method = invocation.getMethodName();
-        String prefix = "DUBBO调用日志:[" + name + "." + method + "]";
+        String prefix = "[traceId:" + traceId + "] DUBBO调用日志:[" + serviceKey + "]";
+        if (sign == null || !sign.equals(signNew)) {
+            log.info(prefix + " 校验sign失败");
+        }
         log.info(prefix + " 入参=>" + JSON.toJSONString(args));
         long start = System.currentTimeMillis();
         Result r = invoker.invoke(invocation);
