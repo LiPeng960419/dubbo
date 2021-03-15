b2df7328a65e96ee80a27e9e0d6fa5f04fbeb5a2
lipeng <lp960419@163.com>
2021-03-15 13:47:14 +0800

    dubbo graypush commit

--- consumer-demo/src/main/java/com/lipeng/consumerdemo/config/DubboReferenceUtils.java
+++ consumer-demo/src/main/java/com/lipeng/consumerdemo/config/DubboReferenceUtils.java
@@ -6,7 +6,11 @@ import com.alibaba.dubbo.config.RegistryConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,19 +19,33 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-public class DubboReferenceConfig implements InitializingBean {
+public class DubboReferenceUtils implements InitializingBean {
 
     @Value("${dubbo.application.name}")
     private String applicationName1;
     private static String applicationName;
 
-    @Value("${dubbo.registries.prod.address}")
-    private String address1;
-    private static String address;
+    @Bean("prodRegistryConfig")
+    @ConfigurationProperties(prefix = "dubbo.registries.prod")
+    public RegistryConfig prodRegistryConfig() {
+        return new RegistryConfig();
+    }
 
-    @Value("${dubbo.registries.gray.address}")
-    private String grayAddress1;
-    private static String grayAddress;
+    @Bean("grayRegistryConfig")
+    @ConfigurationProperties(prefix = "dubbo.registries.gray")
+    public RegistryConfig grayRegistryConfig() {
+        return new RegistryConfig();
+    }
+
+    @Autowired
+    @Qualifier("grayRegistryConfig")
+    private RegistryConfig grayRegistryConfig1;
+    private static RegistryConfig grayRegistryConfig;
+
+    @Autowired
+    @Qualifier("prodRegistryConfig")
+    private RegistryConfig prodRegistryConfig1;
+    private static RegistryConfig prodRegistryConfig;
 
     public static <T> T getDubboBean(Class<T> dubboClasss, String dubboVersion) {
         return getDubboBean(dubboVersion, dubboClasss, false);
@@ -56,20 +74,14 @@ public class DubboReferenceConfig implements InitializingBean {
             ApplicationConfig application = new ApplicationConfig();
             application.setName(applicationName);
             // 连接注册中心配置
-            RegistryConfig registry = new RegistryConfig();
-            registry.setProtocol("zookeeper");
-            if (isGray) {
-                registry.setAddress(grayAddress);
-            } else {
-                registry.setAddress(address);
-            }
+            RegistryConfig registryConfig = isGray ? grayRegistryConfig : prodRegistryConfig;
             // 注意：ReferenceConfig为重对象，内部封装了与注册中心的连接，以及与服务提供方的连接
             // 引用远程服务
             ReferenceConfig reference = new ReferenceConfig();
             reference.setApplication(application);
             // 多个注册中心可以用setRegistries()
             reference.setCheck(false);
-            reference.setRegistry(registry);
+            reference.setRegistry(registryConfig);
             reference.setInterface(dubboClasss);
             reference.setVersion(StringUtils.isEmpty(dubboVersion) ? "1.0.0" : dubboVersion);
             Object obj = reference.get();
@@ -83,8 +95,8 @@ public class DubboReferenceConfig implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         applicationName = applicationName1;
-        address = address1;
-        grayAddress = grayAddress1;
+        prodRegistryConfig = prodRegistryConfig1;
+        grayRegistryConfig = grayRegistryConfig1;
     }
 
 }
