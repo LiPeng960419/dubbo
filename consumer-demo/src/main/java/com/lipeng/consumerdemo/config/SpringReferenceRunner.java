aaa4b79701b03d95dcdf3ef98f9aa8d3f31a7752
lipeng <lp960419@163.com>
2021-03-24 18:25:17 +0800

    dubbo commit

--- consumer-demo/src/main/java/com/lipeng/consumerdemo/config/SpringReferenceRunner.java
+++ consumer-demo/src/main/java/com/lipeng/consumerdemo/config/SpringReferenceRunner.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @Component
@@ -23,36 +24,48 @@ public class SpringReferenceRunner implements CommandLineRunner, ApplicationCont
 
     private static ApplicationContext applicationContext;
 
+    private static final AtomicBoolean init = new AtomicBoolean(false);
+
     /**
      * 如果消费组这里设置reference时 提供者没启动 获取到的reference为null
+     *
      * @param args
      * @throws Exception
      */
     @Override
-    public void run(String... args) throws Exception {
-        String packageName = "com.lipeng.consumerdemo";
-        Reflections f = new Reflections(packageName);
-        Set<Class<?>> set = f.getTypesAnnotatedWith(RestController.class);
-        for (Class<?> c : set) {
-            if (c.isAnnotationPresent(RestController.class)) {
-                Field[] declaredFields = c.getDeclaredFields();
-                for (Field field : declaredFields) {
-                    if (field.isAnnotationPresent(SpringReference.class)) {
-                        field.setAccessible(true);
-                        SpringReference annotation = field.getAnnotation(SpringReference.class);
-                        String mock = annotation.mock();
-                        String stub = annotation.stub();
-                        String version = annotation.version();
-                        ConsumerConfig consumerConfig = new ConsumerConfig();
-                        consumerConfig.setMock(mock);
-                        consumerConfig.setStub(stub);
-                        Object bean = applicationContext.getBean(c);
-                        Class<?> type = field.getType();
-                        field.set(bean, factory.getDubboBean(type, version, consumerConfig));
+    public void run(String... args) {
+        new Thread(() -> {
+            while (!init.get()) {
+                try {
+                    String packageName = "com.lipeng.consumerdemo";
+                    Reflections f = new Reflections(packageName);
+                    Set<Class<?>> set = f.getTypesAnnotatedWith(RestController.class);
+                    for (Class<?> c : set) {
+                        if (c.isAnnotationPresent(RestController.class)) {
+                            Field[] declaredFields = c.getDeclaredFields();
+                            for (Field field : declaredFields) {
+                                if (field.isAnnotationPresent(SpringReference.class)) {
+                                    field.setAccessible(true);
+                                    SpringReference annotation = field.getAnnotation(SpringReference.class);
+                                    String mock = annotation.mock();
+                                    String stub = annotation.stub();
+                                    String version = annotation.version();
+                                    ConsumerConfig consumerConfig = new ConsumerConfig();
+                                    consumerConfig.setMock(mock);
+                                    consumerConfig.setStub(stub);
+                                    Object bean = applicationContext.getBean(c);
+                                    Class<?> type = field.getType();
+                                    field.set(bean, factory.getDubboBean(type, version, consumerConfig));
+                                }
+                            }
+                        }
                     }
+                    init.set(true);
+                } catch (Exception e) {
+                    log.error("SpringReference init error", e);
                 }
             }
-        }
+        }).start();
     }
 
     @Override
